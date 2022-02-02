1ba3a20c1fe197f327055e011c896c89f5c71b85
Kaushik Chemburkar <attherate@gmail.com>
2022-02-02 16:22:05 +1100

    how to see the request object log in the terminal
    Signed-off-by: Kaushik Chemburkar <attherate@gmail.com>

--- Basics/http.js
+++ Basics/http.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer ((request , response) => {
-    console.log(request);
+    console.log(response);
     response.write('Hello World');
     response.end();
 })
