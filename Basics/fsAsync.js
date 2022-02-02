97e3060a04e6802381a90a5547fc5a2abf6e0934
Kaushik Chemburkar <48584376+k4u5hik@users.noreply.github.com>
2022-02-02 12:09:38 +1100

    fs Async Operation
    - Callbacks info in the previous commit description
    - Webstorm commits showing as unverified
    
    Signed-off-by: Kaushik Chemburkar <48584376+k4u5hik@users.noreply.github.com>

---
+++ Basics/fsAsync.js
@@ -0,0 +1,63 @@
+const {readFile, writeFile} = require('fs');
+
+/*
+
+one way to do it
+
+readFile('./content/1.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});*/
+
+/*
+Returns buffer
+
+readFile('./content/1.txt', (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});*/
+
+/*
+readFile('./content/1.txt', (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data
+            .toString()
+            .split('\n'))
+    }
+});*/
+
+console.log('start')
+readFile('./content/1.txt', 'utf8', (err, result1) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+    const first = result1
+    readFile('./content/subFolder/2.txt', 'utf8', (err, result2) => {
+        if (err) {
+            console.log(err)
+            return
+        }
+        const second = result2
+        writeFile(
+            './content/result-async.txt',
+            `Here is the result : ${first}, ${second}`,
+            (err, result2) => {
+                if (err) {
+                    console.log(err)
+                    return
+                }
+                console.log('done with this task')
+            }
+        )
+    })
+})
+console.log('starting next task')
